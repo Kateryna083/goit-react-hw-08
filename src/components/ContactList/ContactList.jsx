a2efd2cd2dced437f6464728a56390cca5c8b66f
Kateryna083 <katyasmile083@gmail.com>
2024-09-15 11:12:35 +0300

    components

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,22 @@
+import { useSelector } from "react-redux";
+import { selectContacts } from "../../redux/contactsSlice";
+import { selectFilter } from "../../redux/filtersSlice";
 import Contact from "../Contact/Contact";
 
-// import css from './'
+// import css from './ContactList.module.css'
 
-export default function ContactList({ contacts, onDelete }) {
+export default function ContactList() {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectFilter);
+
+  const visibleContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
   return (
     <ul>
-      {contacts.map((contact) => (
+      {visibleContacts.map((contact) => (
         <li key={contact.id}>
-          <Contact contact={contact} onDelete={onDelete} />
+          <Contact item={contact} />
         </li>
       ))}
     </ul>
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,14 +1,22 @@
-// import css from './'
+import { useDispatch, useSelector } from "react-redux";
+import { setFilter, selectFilter } from "../../redux/filtersSlice";
+import { FaSearch } from "react-icons/fa";
 
-export default function SearchBox({ value, onFilter }) {
+// import css from './SearchBox.module.css'
+
+export default function SearchBox() {
+  const dispatch = useDispatch();
+  const filterValue = useSelector(selectFilter);
+
+  const handleChange = (event) => {
+    dispatch(setFilter(event.target.value));
+  };
   return (
     <div>
-      <p>Find contacts by name</p>
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => onFilter(e.target.value)}
-      ></input>
+      <label htmlFor="searchbox">
+        <FaSearch size="12" /> Find contacts by name
+      </label>
+      <input type="text" value={filterValue} onChange={handleChange}></input>
     </div>
   );
 }
